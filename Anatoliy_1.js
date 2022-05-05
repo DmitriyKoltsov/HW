d33cbf50f365fb36a1bf7603b8bfdf0892e0c1c1
DmitriyKoltsov <koltsovDI@bk.ru>
2022-05-05 20:24:58 +0400

    add HW jt Anatoliya

---
+++ Anatoliy_1.js
@@ -0,0 +1,70 @@
+    // Написать скриптик, который сосчитает и выведет результат от возведения 2 в степень 10, начиная со степени 1//
+    // Преобразовать 1 задачу в функцию, принимающую на вход степень, в которую будет возводиться число 2//
+
+// let a = 2;
+// let b = 10;
+
+// for (b = 1; b <=10; b++) {
+     
+//     console.log("c = " + Math.pow(a,b));
+//     }
+
+
+// let a = 2;
+// let b = 1;
+
+// while (b >= 1 && b <= 10) {
+            
+//     console.log("c = " + Math.pow(a,b));
+//     b++
+// }
+  
+
+
+// let a = 2;
+// let b = 1;
+
+
+// const checkAge = function(n) {
+//     n = Number(n)
+//     if (!isNaN(n)) 
+//     {
+//         console.log(n)
+   
+//         for (b = 1; b <= n; b++) 
+//         {
+               
+//             console.log("c = " + Math.pow(a,b));   
+//         }
+//     }
+//             else
+//             {
+//                 console.log("Ne chislo")
+//             }
+    
+
+// }
+// checkAge(3)
+
+
+
+
+// let a = 2;
+// let b = 0;
+
+// const checkAge = function(n){
+//     age = Number(n)
+//     if (!isNaN(n)){
+
+// while (b >= 0 && b <= n) {
+            
+//     console.log("c = " + Math.pow(a,b));
+//         b++
+//     }
+//     }
+//     else
+//     {
+//      console.log("Ne chislo")
+//     }
+// }
+// checkAge(10)
