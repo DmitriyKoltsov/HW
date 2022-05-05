577ab92e5b2126d2e8c3d1b661ce1f5e4fcc3a59
DmitriyKoltsov <koltsovDI@bk.ru>
2022-05-05 20:26:40 +0400

    add HW 2 ot Anatoliya

---
+++ Anatoliy_2.js
@@ -0,0 +1,57 @@
+    // Написать скрипт, который выведет 5 строк в консоль таким образом, чтобы в первой строчке выводилось :), во второй :):) и так далее
+    // Пример в консоли:
+    // :)
+    // :):)
+    // :):):)
+    // :):):):)
+    // :):):):):)
+
+    // Преобразовать 2 задачу в функцию, принимающую на вход строку, которая и будет выводиться в консоль (как в условии смайлик), а также количество строк для вывода 
+    // e.g. function printSmile(stroka, numberOfRows)
+
+// let a = "";
+// let b = 1;
+
+// for (b = 1; b <= 10; b++) {
+//         a += ":)";
+        
+//         console.log(a);
+//     }
+
+
+// let a = "";
+// let b = 1;
+
+// while (b >= 1 && b<=15) {
+    
+//         a += ":)";
+//         b++
+                
+//         console.log(a);
+// }
+
+
+
+// let a = "";
+// function printSmile(stroka, numberOfRows) {
+//     for (i = 1; i <= numberOfRows; i++){
+//     a += stroka
+//     console.log(a)
+    
+//     }
+// }
+// printSmile (":)", 6)
+
+
+// let a = "";
+// let b = 1;
+
+// function printSmile(stroka, numberOfRows){
+// while (b >= 1 && b <= numberOfRows) {
+//     a += stroka
+//     b++
+                
+//         console.log(a);
+//     }
+// }
+// printSmile(":)", 6)
